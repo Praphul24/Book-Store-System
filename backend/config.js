236b4d3ced414864e84761c51dcce48aa3961163
Praphul Yadav <praphulyadav150@gmail.com>
2023-12-25 19:28:37 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://praphulyadav:dm125.working@books-store-mern.vz8h5yt.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
